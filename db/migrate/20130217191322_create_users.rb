c5d9ee3003dd60824d1407d3c2b57370a698b0f6
Kyle Demeule <kyle.demeule@gmail.com>
2013-02-17 11:24:01 -0800

    Initial user model code.

---
+++ db/migrate/20130217191322_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :display_name
+      t.string :email
+      t.string :password_digest
+      t.string :status
+      t.string :remember_token
+      t.boolean :logged_in
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index  :users, :remember_token
+  end
+end
