5b805d06cf298774c55f892fc8cb6fb63db51874
Kyle Demeule <kyle.demeule@gmail.com>
2013-02-17 11:04:13 -0800

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClioStatusboard::Application.config.secret_token = '638af5ea74ee400bae37a110ebba7367831cd81f054239424b14ad9ee371668c221bb612d38190036019d36ba9e80a0b4fbbee56091afc55a035122e1d46b030'
