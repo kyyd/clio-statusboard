782fe5d8e8fc8f2b76a796ca26f443acc7d30745
Kyle Demeule <kyle.demeule@gmail.com>
2013-02-17 16:17:29 -0800

    Adding some basic controller tests.

---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+    test "should get new" do
+        get :new
+        assert_response :success
+    end
+
+    test "should create user" do
+        assert_difference('User.count') do
+            post :create, :user => { :display_name => 'Mickey Mouse',
+                                     :email => "mickey@disney.com",
+                                     :password => "minnie",
+                                     :password_confirmation => "minnie"}
+        end
+        assert_redirected_to user_path(assigns(:user))
+    end
+
+    test "should not be able to get index when not logged in" do
+        get :index
+        assert_redirected_to("/signin")
+    end
+
+end
